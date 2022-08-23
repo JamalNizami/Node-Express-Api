2166fe6e8ee1570e875a17c7383c4b2224a2a37a
JamalNizami <jamal.anjum2001@gmail.com>
2022-08-23 21:47:31 +0500

    update files

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors =  require('colors')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
+// error said  connectDB() is not a function 
+connectDB()
+
 const app = express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
