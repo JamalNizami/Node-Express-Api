495cb8d244550337b0f5b9b618028842c402afb1
JamalNizami <jamal.anjum2001@gmail.com>
2022-08-29 11:31:19 +0500

    AuthMiddleware Added

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../model/userModel')
+
+const protect = asyncHandler(async (req,res,next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer") ){
+        try {
+            // Get token from Header 
+            token = req.headers.authorization.split(' ')[1]
+
+            // verify the token 
+            const decoded = jwt.verify(token, process.env.JWY_SECRET)
+
+            // Get token from user
+
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+
+        } catch (error) {
+            console.log(error);
+            res.status(401)
+            throw new Error("Not Authorized")
+        }
+    } else {
+        res.status(401)
+        throw new Error("Not Authorized , No token")
+    }
+} )
+
+
+module.exports = {
+    protect,
+}
+
